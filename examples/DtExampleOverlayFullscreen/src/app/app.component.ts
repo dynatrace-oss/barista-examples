2a4c03c0bdd59d59cff673f1be14d535989d0973
Jenkins <jenkins@ls-ub-bm987a1fv.dynatrace.vmta>
2020-04-09 16:32:32 +0200

    Automatic update of examples

---
+++ examples/DtExampleOverlayFullscreen/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barista-demo-application',
+  template: '<dt-example-overlay-fullscreen></dt-example-overlay-fullscreen>',
+  styleUrls: [ './app.component.scss', ]
+})
+export class AppComponent {}
