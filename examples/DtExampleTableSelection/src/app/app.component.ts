a64f6c3d870e6255eaa97ab3b75372f6615ef44a
Jenkins <jenkins@ls-ub-bm987a1fv.dynatrace.vmta>
2020-07-28 08:01:10 +0200

    Automatic update of examples

---
+++ examples/DtExampleTableSelection/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barista-demo-application',
+  template: '<dt-example-table-selectable-column></dt-example-table-selectable-column>',
+  styleUrls: [ './app.component.scss', ]
+})
+export class AppComponent {}
