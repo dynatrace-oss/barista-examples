329fa1fc39476a3e054d02f112097130b68caf5b
Jenkins <jenkins@ls-ub-bm987a1fv.dynatrace.vmta>
2020-11-13 10:16:37 +0100

    Automatic update of examples

---
+++ examples/DtExampleFilterFieldInfiniteDataDepth/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barista-demo-application',
+  template: '<dt-example-filter-field-infinite-data-depth-filter-field></dt-example-filter-field-infinite-data-depth-filter-field>',
+  styleUrls: [ './app.component.scss', ]
+})
+export class AppComponent {}
---
+++ examples/DtExampleSelectCustomValueTemplate/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barista-demo-application',
+  template: '<dt-example-select-custom-value-template-example></dt-example-select-custom-value-template-example>',
+  styleUrls: [ './app.component.scss', ]
+})
+export class AppComponent {}
