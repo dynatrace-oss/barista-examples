acc4d9451e3408e796e131fad079a0779540b6be
Jenkins <jenkins@ls-ub-bm987a1fv.dynatrace.vmta>
2021-02-09 14:16:32 +0100

    Automatic update of examples

---
+++ examples/DtExampleFilterFieldDefaultSearch/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barista-demo-application',
+  template: '<dt-example-filter-field-default-search></dt-example-filter-field-default-search>',
+  styleUrls: [ './app.component.scss', ]
+})
+export class AppComponent {}
--- examples/DtExampleFilterFieldValidator/src/filter-field/filter-field-validator-example/filter-field-validator-example.ts
+++ examples/DtExampleFilterFieldValidator/src/filter-field/filter-field-validator-example/filter-field-validator-example.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2020 Dynatrace LLC
+ * Copyright 2021 Dynatrace LLC
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -30,7 +30,11 @@ export class DtExampleFilterFieldValidator {
         autocomplete: [
           {
             name: 'Custom',
-            suggestions: ['Linz', 'Vienna', 'Graz'],
+            suggestions: [
+              { name: 'Linz' },
+              { name: 'Vienna' },
+              { name: 'Graz' },
+            ],
             validators: [
               { validatorFn: Validators.required, error: 'is required' },
             ],
