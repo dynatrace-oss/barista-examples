203dad915805bbc97fa4fa3231cab6ce1318d8b7
Jenkins <jenkins@ls-ub-bm987a1fv.dynatrace.vmta>
2021-03-30 14:40:53 +0200

    Automatic update of examples

---
+++ examples/DtExampleQuickFilterCustomParser/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barista-demo-application',
+  template: '<dt-example-quick-filter-custom-parser></dt-example-quick-filter-custom-parser>',
+  styleUrls: [ './app.component.scss', ]
+})
+export class AppComponent {}
