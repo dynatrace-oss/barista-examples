eefaaf9b724a88bec1dce5449833730afebd50ba
Thomas Heller <thomas.heller@dynatrace.com>
2021-04-21 16:32:34 +0200

    fix filter-field example

---
+++ examples/DtExampleFilterFieldCustomPlaceholder/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barista-demo-application',
+  template: '<dt-example-filter-field-filter-field-custom-placeholder></dt-example-filter-field-filter-field-custom-placeholder>',
+  styleUrls: [ './app.component.scss', ]
+})
+export class AppComponent {}
---
+++ examples/DtExampleFilterFieldCustomPlaceholder/src/filter-field/filter-field-examples.module.ts
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * Copyright 2021 Dynatrace LLC
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { NgModule } from '@angular/core';
+import { DtFilterFieldModule } from '@dynatrace/barista-components/filter-field';
+import { DtInputModule } from '@dynatrace/barista-components/input';
+import { DtExampleFilterFieldCustomPlaceholder } from './filter-field-custom-placeholder-example/filter-field-custom-placeholder-example';
+@NgModule({
+    imports: [DtFilterFieldModule, DtInputModule],
+    declarations: [
+        DtExampleFilterFieldCustomPlaceholder,
+    ],
+    exports: [DtExampleFilterFieldCustomPlaceholder]
+})
+export class DtFilterFieldExamplesModule {
+}
