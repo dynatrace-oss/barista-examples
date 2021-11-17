3d7bbfb420f5fd8c8ee3046a3c0a02f874d9d1a2
Jenkins <jenkins@ls-ub-bm987a1fv.dynatrace.vmta>
2021-11-17 17:29:20 +0100

    Automatic update of examples

---
+++ examples/DtExampleStackedSeriesChartHeatField/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barista-demo-application',
+  template: '<dt-example-heat-field-stacked-series-chart></dt-example-heat-field-stacked-series-chart>',
+  styleUrls: [ './app.component.scss', ]
+})
+export class AppComponent {}
