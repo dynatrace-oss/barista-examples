458e03fcd490120a76570a1a85a7c1c53450d24d
Jenkins <jenkins@ls-ub-bm987a1fv.dynatrace.vmta>
2022-02-22 11:07:04 +0100

    Automatic update of examples

--- examples/DtExampleStackedSeriesChartLinear/src/app/app.component.ts
+++ examples/DtExampleStackedSeriesChartLinear/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'barista-demo-application',
-  template: '<dt-example-stacked-series-chart-date-barista></dt-example-stacked-series-chart-date-barista>',
+  template: '<dt-example-stacked-series-chart-linear-barista></dt-example-stacked-series-chart-linear-barista>',
   styleUrls: [ './app.component.scss', ]
 })
 export class AppComponent {}
