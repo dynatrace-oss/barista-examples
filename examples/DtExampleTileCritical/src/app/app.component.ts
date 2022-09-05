b15f3dbe51e7a26dc24c6e86ee8dfcefbe9b71b4
Jenkins <jenkins@ls-ub-bm987a1fv.dynatrace.vmta>
2022-09-05 15:23:20 +0200

    Automatic update of examples

---
+++ examples/DtExampleTileCritical/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barista-demo-application',
+  template: '<dt-example-tile-critical></dt-example-tile-critical>',
+  styleUrls: [ './app.component.scss', ]
+})
+export class AppComponent {}
